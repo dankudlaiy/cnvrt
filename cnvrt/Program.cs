387523c45140eac7f73cc41023bb53053524281b
Danylo Kudlai <dan.kudlai@techfabric.com>
2023-11-15 16:28:05 +0100

    using microsoft print to pdf as a converting technique

--- cnvrt/Program.cs
+++ cnvrt/Program.cs
@@ -1,19 +1,11 @@
-using iText.IO.Font;
-using iText.IO.Font.Constants;
-using iText.Kernel.Font;
-using OfficeOpenXml;
-using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
-using iText.Layout.Font;
-using Xceed.Words.NET;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace cnvrt;
 
 internal static class Program
 {
-    private static bool _xlmsg = true;
-    
     [STAThread]
     private static void Main()
     {
@@ -24,12 +16,16 @@ internal static class Program
         using var folderBrowserDialog = new FolderBrowserDialog();
         
         var result = folderBrowserDialog.ShowDialog();
-            
+        
         if (result == DialogResult.OK)
         {
             var selectedFolder = folderBrowserDialog.SelectedPath;
 
             Console.WriteLine($"Selected Folder: {selectedFolder}");
+            
+            var excelApp = new Excel.Application();
+            
+            var wordApp = new Word.Application();
                 
             foreach (var filePath in Directory.GetFiles(selectedFolder))
             {
@@ -39,7 +35,7 @@ internal static class Program
                 
                     try
                     {
-                        ConvertExcelToPdf(filePath, $"{filePath}.pdf");
+                        ConvertExcelToPdf(excelApp, filePath, $"{filePath}.pdf");
                     
                         Console.WriteLine(" | Converted");
                     }
@@ -55,12 +51,8 @@ internal static class Program
                 
                 try
                 {
-                    var left = Console.CursorLeft;
-                    
-                    ConvertDocxToPdf(filePath, $"{filePath}.pdf");
-                    
-                    if(_xlmsg) ClearXlMsg(left);
-                    
+                    ConvertDocxToPdf(wordApp, filePath, $"{filePath}.pdf");
+
                     Console.WriteLine(" | Converted");
                 }
                 catch (Exception ex)
@@ -68,7 +60,13 @@ internal static class Program
                     Console.WriteLine($" | Error | {ex.Message}");
                 }
             }
+
+            excelApp.Quit();
+            wordApp.Quit();
             
+            Marshal.ReleaseComObject(excelApp);
+            Marshal.ReleaseComObject(wordApp);
+
             Console.WriteLine("\nend");
             Console.ReadLine();
         }
@@ -78,54 +76,31 @@ internal static class Program
         }
     }
     
-    private static void ConvertExcelToPdf(string excelFilePath, string pdfFilePath)
+    private static void ConvertExcelToPdf(Excel.Application excelApp, string excelFilePath, string pdfFilePath)
     {
-        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var workbooks = excelApp.Workbooks;
+        var workbook = workbooks.Open(excelFilePath);
         
-        using var package = new ExcelPackage(new FileInfo(excelFilePath));
+        workbook.PrintOut(1, 2147483647, 1, false, 
+            "Microsoft Print to PDF", true, false, pdfFilePath);
         
-        using var pdfDocument = new PdfDocument(new PdfWriter(pdfFilePath));
+        workbook.Close();
         
-        using var document = new Document(pdfDocument);
-        
-        document.SetFont(GetFont());
-        
-        foreach (var worksheet in package.Workbook.Worksheets)
-        {
-            document.Add(new Paragraph(worksheet.Name));
-
-            var table = new Table(worksheet.Dimension.Columns);
-            
-            for (var row = 1; row <= worksheet.Dimension.Rows; row++)
-            {
-                for (var col = 1; col <= worksheet.Dimension.Columns; col++)
-                {
-                    var cellValue = worksheet.Cells[row, col].Text;
-                    table.AddCell(cellValue);
-                }
-            }
-
-            document.Add(table);
-            document.Add(new Paragraph("\n"));
-        }
+        Marshal.ReleaseComObject(workbook);
+        Marshal.ReleaseComObject(workbooks);
     }
 
-    private static void ConvertDocxToPdf(string docxFilePath, string pdfFilePath)
+    private static void ConvertDocxToPdf(Word.Application wordApp, string docxFilePath, string pdfFilePath)
     {
-        using var doc = DocX.Load(docxFilePath);
+        var documents = wordApp.Documents;
+        var document = documents.Open(docxFilePath);
         
-        using var pdfWriter = new PdfWriter(pdfFilePath);
+        document.PrintOut(Type.Missing, Type.Missing, Type.Missing, pdfFilePath, "Microsoft Print to PDF");
         
-        using var pdfDocument = new PdfDocument(pdfWriter);
+        document.Close();
         
-        using var document = new Document(pdfDocument);
-
-        document.SetFont(GetFont());
-        
-        foreach (var paragraph in doc.Paragraphs)
-        {
-            document.Add(new Paragraph(paragraph.Text));
-        }
+        Marshal.ReleaseComObject(document);
+        Marshal.ReleaseComObject(documents);
     }
 
     private static bool IsExcelFile(string filePath)
@@ -140,34 +115,5 @@ internal static class Program
         var extension = Path.GetExtension(filePath).ToLower();
         
         return extension is ".doc" or ".docx";
-    }
-    
-    private static void ClearXlMsg(int left)
-    {
-        for (var i = 0; i < 4; i++)
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write(new string(' ', 67));
-        }
-        
-        Console.SetCursorPosition(left, Console.CursorTop - 1);
-        
-        Console.Write(new string(' ', 67));
-        
-        Console.SetCursorPosition(left, Console.CursorTop);
-
-        _xlmsg = false;
-    }
-
-    private static PdfFont GetFont()
-    {
-        var dir = Directory.GetCurrentDirectory();
-
-        var path = Path.Combine(dir, "arial-unicode-ms.ttf");
-
-        var fontProgram = FontProgramFactory.CreateFont(path);
-
-        return PdfFontFactory.CreateFont(fontProgram, PdfEncodings.IDENTITY_H, 
-            PdfFontFactory.EmbeddingStrategy.FORCE_EMBEDDED);
     }
 }
